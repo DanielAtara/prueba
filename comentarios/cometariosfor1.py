ed4962e2ea443622b5b4fb49b1e590d0c5eed26b
daniel <dazataras@gmail.com>
2023-04-26 16:50:00 -0500

    organizacion documentos y subida de comentarios

---
+++ comentarios/cometariosfor1.py
@@ -0,0 +1,14 @@
+for i in range(11):#se ingresa el limite de ciclo for 
+    if i%3==0:# se evaula que numeros son multiplos de 3 
+        print(f'{i} es multiplo de 3')# se imprimen los numeros multiplos de 3 
+    else:
+        print(i)#se siguen imprimiendo los numeros hasta el limite anteriormente planteado 
+
+for j in range(1,11): # se da un numero de punto de partida y uno de finalizacion 
+    print(j) # se ejecuta lo anterimente nombrado 
+
+for k in range(0,11,2): # se da un numero de punto de partida y uno de finalizacion y se incrementa de dos en dos 
+    print(k)
+
+for i in range(20,0,-2): # se da un numero de punto de partida y uno de finalizacion y se le da la asginacion que sean descontados los numeros de dos en dos 
+    print(i)
---
+++ ejercicios en clase/bucles/serie.py
@@ -0,0 +1,10 @@
+num1=int(input("ingrese el numero" ))
+num2=0
+cont=0
+while num2<num1:
+    num2+=1
+    print(num2)
+    if num2%7 == 0:
+        print(F"{num2}este numero es multiplo ")
+    
+    
